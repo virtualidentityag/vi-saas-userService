5ea1407052aec5c1739bf2f90508992adda657a3
tkuzynow <tomasz.kuzynowski@gmail.com>
2024-08-22 15:52:52 +0200

    fix: delete rooms even if user is already deleted from MariaDB

--- src/main/java/de/caritas/cob/userservice/api/adapters/rocketchat/RocketChatService.java
+++ src/main/java/de/caritas/cob/userservice/api/adapters/rocketchat/RocketChatService.java
@@ -804,7 +804,6 @@ public class RocketChatService implements MessageClient {
    * @return al members of the group
    * @deprecated use getChatUsers
    */
-  @Deprecated
   public List<GroupMemberDTO> getMembersOfGroup(String rcGroupId)
       throws RocketChatGetGroupMembersException {
 
