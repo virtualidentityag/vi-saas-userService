b0dbbd8d65ef8a1524f057625a726e5fb564fc21
tkuzynow <tomasz.kuzynowski@gmail.com>
2025-01-09 17:08:44 +0100

    fix: revert change in spring security to avoid issues with tenantFilter ordering

--- src/main/java/de/caritas/cob/userservice/api/config/auth/SecurityConfig.java
+++ src/main/java/de/caritas/cob/userservice/api/config/auth/SecurityConfig.java
@@ -10,7 +10,6 @@ import de.caritas.cob.userservice.api.service.security.JwtAuthConverter;
 import de.caritas.cob.userservice.api.service.security.JwtAuthConverterProperties;
 import lombok.RequiredArgsConstructor;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
-import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticatedActionsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
