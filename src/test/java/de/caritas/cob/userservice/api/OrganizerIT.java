b22b4287393c5082aab272759ba2aeb7a4a3af4a
tkuzynow <tomasz.kuzynowski@gmail.com>
2025-01-30 12:09:23 +0100

    chore: add disabled test that had AOT issues not related to test itself

--- src/test/java/de/caritas/cob/userservice/api/OrganizerIT.java
+++ src/test/java/de/caritas/cob/userservice/api/OrganizerIT.java
@@ -14,6 +14,7 @@ import java.time.temporal.ChronoUnit;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -26,6 +27,7 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest
 @ActiveProfiles("testing")
 @AutoConfigureTestDatabase(replace = Replace.ANY)
+@Disabled
 class OrganizerIT {
 
   private static final EasyRandom easyRandom = new EasyRandom();
